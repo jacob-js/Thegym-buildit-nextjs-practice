391c34a7d21e5232af28234e4524da9708229567
Jacob Dev <mercihabam@gmail.com>
2022-10-25 16:19:13 +0200

    add about hero section

---
+++ components/About/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+function About() {
+  return (
+    <div className='bg-light p-32 pb-40 pt-64'>
+        <Hero />
+    </div>
+  )
+}
+
+export default About
